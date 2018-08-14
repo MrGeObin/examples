74ab00899476802ae5fab9a0b58f82b435b9a07a
Geovanni Obin <mrgeobin@gmail.com>
2018-08-13 17:38:10 -0700

    adding the transition to the Btn Menu

---
+++ school page/js/btn-menu.js
@@ -0,0 +1,6 @@
+var nav = document.getElementById('nav');
+var btnMenu = document.getElementById('btn-menu');
+
+btnMenu.addEventListener('click', function () {
+    nav.classList.toggle('mostrar');
+})
