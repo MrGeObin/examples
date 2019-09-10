047f58bdb752eb08c6fb673b085b7e17e136ab96
Elmer Obin <mrgeobin@gmail.com>
2019-09-09 22:11:24 -0700

    testing

---
+++ JS/js/index.js
@@ -0,0 +1,6 @@
+let allow = prompt('DO YOU WANT TO SEE THE PICTURE?')
+
+let go = allow = "yes" ? 'ok go to see the picture' 
+                        : 'ok u wont see the picture'
+
+alert(go)
--- school page/js/btn-menu.js
+++
@@ -1,6 +0,0 @@
-var nav = document.getElementById('nav');
-var btnMenu = document.getElementById('btn-menu');
-
-btnMenu.addEventListener('click', function () {
-    nav.classList.toggle('mostrar');
-})
